5aca963aac833d01ab5ae1a3a2d6c664dd2fbac0
Daniel A. White <daniel@whitefamily.in>
2025-06-22 16:35:50 -0400

    sync objects

--- controlplane/src/controllers/macros/sync_objects.rs
+++ controlplane/src/controllers/macros/sync_objects.rs
@@ -1,14 +1,25 @@
 #[macro_export]
 macro_rules! sync_objects {
-    ($object_type:ty, $client:ident, $queue:ident, $template_value:ty, $template:literal) => {{
-        use gtmpl::Template;
-        use kube::Api;
-        use tokio::spawn;
-        use tokio::sync::broadcast::Receiver;
-        use tracing::{debug, info, warn};
+    ($object_type:ty, $client:ident, $queue:ident, $template_value:ty, $template:ident) => {{
         use $crate::objects::{ObjectRef, SyncObjectAction, SyncObjectAction::*};
+        use gtmpl::{Context, Template};
+        use gtmpl_value::Value;
+        use kube::{ Api, Resource, ResourceExt };
+        use kube::api::{ Patch, ObjectMeta };
+        use k8s_openapi::DeepMerge;
+        use tokio::{select, spawn};
+        use tokio::sync::broadcast::Receiver;
         use tokio::signal::ctrl_c;
-        use tokio::select;
+        use tracing::{debug, info, warn, trace};
+        use std::collections::BTreeMap;
+        use kubera_api::constants::{MANAGED_BY_LABEL, MANAGED_BY_VALUE};
+
+        const _: () = {
+            fn assert_impl<T: Resource + ResourceExt>() {}
+            fn assert_type_bounds() {
+                assert_impl::<$object_type>();
+            }
+        };
 
         let client = $client.clone();
         let mut queue_recv: Receiver<SyncObjectAction<$template_value>> = $queue.subscribe();
@@ -18,6 +29,27 @@ macro_rules! sync_objects {
             template.parse($template).expect("Unable to parse template");
             template
         };
+
+        fn render_object<V: Into<Value>>(template: &Template, object_ref: &ObjectRef, value: V) -> $object_type {
+            let context = Context::from(value);
+            let yaml = template.render(&context).expect("Unable to render template");
+            let mut object: $object_type = serde_yaml::from_str(&yaml)
+                .expect("Unable to deserialize rendered template into object");
+
+            let new_metadata = ObjectMeta {
+                name: Some(object_ref.name().to_string()),
+                namespace: object_ref.namespace().as_ref().cloned(),
+                labels: Some(BTreeMap::from([
+                    (MANAGED_BY_LABEL.to_string(), MANAGED_BY_VALUE.to_string())
+                ])),
+                ..Default::default()
+            };
+            let mut existing_metadata = object.metadata;
+            existing_metadata.merge_from(new_metadata);
+            object.metadata = existing_metadata;
+
+            object
+        }
 
         debug!(
             "Spawning controller for writing {} objects",
@@ -58,12 +90,25 @@ macro_rules! sync_objects {
 
                 match (&action, exists) {
                     (Upsert((object_ref, value)), false) => {
+                        let object = render_object(&template, object_ref, value);
+                        info!("Creating object: {}", object_ref);
+                        api.create(&Default::default(), &object)
+                            .await
+                            .map_err(|e| warn!("Failed to create object: {}: {}", object_ref, e))
+                            .ok();
                     }
                     (Upsert((object_ref, value)), true) => {
+                        let object = render_object(&template, object_ref, value);
+                        info!("Patching object: {}", object_ref);
+                        api.patch(object_ref.name(), &Default::default(), &Patch::Strategic(object))
+                            .await
+                            .map_err(|e| warn!("Failed to patch object: {}: {}", object_ref, e))
+                            .ok();
                     }
                     (SyncObjectAction::Delete(object_ref), true) => {
                         info!("Deleting object: {}", object_ref);
-                        api.delete(object_ref.name(), &Default::default()).await
+                        api.delete(object_ref.name(), &Default::default())
+                            .await
                             .map_err(|e| warn!("Failed to delete object: {}: {}", object_ref, e))
                             .ok();
                     }
--- controlplane/src/controllers/mod.rs
+++ controlplane/src/controllers/mod.rs
@@ -52,20 +52,20 @@ pub async fn run(ipc_services: IpcServices) -> Result<()> {
     sync_gateway_configuration(&client, &gateway_config_maps, &gateway_configurations);
     generate_gateway_services(&gateways, ipc_services.clone());
 
-    let (tx, rx) = tokio::sync::broadcast::channel(100);
+    //  let (tx, rx) = tokio::sync::broadcast::channel(100);
 
-    sync_objects!(Service, client, tx, String, "service: {{.}}");
+    //  sync_objects!(Service, client, tx, String, test);
 
-    let r = ObjectRef::new_builder()
-        .of_kind::<Service>()
-        .namespace(Some("default".to_string()))
-        .name("echo-server")
-        .build()
-        .expect("Failed to build ObjectRef");
-
-    let msg = SyncObjectAction::Delete(r);
-
-    tx.send(msg).expect("Failed to send message");
+    // let r = ObjectRef::new_builder()
+    //     .of_kind::<Service>()
+    //     .namespace(Some("default".to_string()))
+    //     .name("echo-server")
+    //     .build()
+    //     .expect("Failed to build ObjectRef");
+    //
+    // let msg = SyncObjectAction::Upsert((r, "test".to_string()));
+    //
+    // tx.send(msg).expect("Failed to send message");
 
     ctrl_c().await;
 
